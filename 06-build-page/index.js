0ecb0f307f68fc8a4273c40ade4d3994617434bb
Sansrona <kingJaM00@yandex.ru>
2022-11-10 10:05:38 +0600

    finish: task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,77 +1,77 @@
 const fs = require('node:fs');
+const fsPromise = require('node:fs/promises');
 const path = require('node:path');
 
 
 const projectDist = path.resolve(__dirname, 'project-dist');
-const assetsDir = path.resolve(__dirname, 'project-dist/assets');
-const stylesFile = path.resolve(__dirname, 'project-dist/style.css');
-const htmlFile = path.resolve(__dirname, 'project-dist/index.html');
+const assetsDir = path.join(projectDist, 'assets');
+const stylesFile = path.join(projectDist, 'style.css');
+const htmlFile = path.join(projectDist, 'index.html');
 
-fs.mkdir(projectDist, { recursive: true }, (err) => {
-    if (err) console.log(err);
-})
-
-fs.mkdir(assetsDir, { recursive: true }, (err) => {
-    if (err) console.log(err);
-})
-
-fs.writeFile(stylesFile, '', (err) => {
-    if (err) console.log(err);
-})
-
-fs.readdir(path.resolve(__dirname, 'assets'), { withFileTypes: true }, (_, dirs) => {
-    for (let dir of dirs) {
-        fs.mkdir(path.join(assetsDir, `${dir.name}`), { recursive: true }, (err) => {
-            console.log(err);
-        });
-        fs.readdir(path.join(assetsDir, `${dir.name}`), { withFileTypes: true }, (_, files) => {
-            for (let file of files) {
-                fs.unlink(path.join(assetsDir, `${dir.name}`, `${file.name}`), (err) => {
-                    if (err) console.log(err);
-                })
-
-            }
-        });
-
-        fs.readdir(path.resolve(__dirname, `assets/${dir.name}`), { withFileTypes: true }, (_, files) => {
-            for (let file of files) {
-                fs.copyFile(path.resolve(__dirname, `assets/${dir.name}/${file.name}`),
-                    path.resolve(assetsDir, `${dir.name}/${file.name}`), (ee) => {
-                        if (ee) console.log(err);
-                    }
-                );
-            }
-        })
-    }
-});
-
-fs.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true }, (_, files) => {
+async function clearDir(dir) {
+    console.log(dir);
+    let files = await fsPromise.readdir(dir);
     for (let file of files) {
-        if (file.isFile() && path.extname(file.name) === '.css') {
-            fs.readFile(path.resolve(__dirname, `styles/${file.name}`), (ee, data) => {
-                fs.appendFile(stylesFile, data, (e) => {
-                    if (e) console.log(e);
-                })
+        let filePath = path.join(dir, file);
+        let stat = await fsPromise.stat(filePath);
+        if (stat.isFile()) {
+            await fsPromise.unlink(filePath);
+        } else {
+            await clearDir(filePath);
+            await fsPromise.rmdir(filePath);
+        }
+    }
+}
+
+async function copyDir(targetDir, newDir) {
+    await fsPromise.mkdir(newDir, { recursive: true });
+    let files = await fsPromise.readdir(targetDir);
+    for (let file of files) {
+        let filePath = path.join(targetDir, file);
+        let stat = await fsPromise.stat(filePath);
+        if (stat.isFile()) {
+            await fsPromise.copyFile(path.join(targetDir, file), path.join(newDir, file));
+        } else {
+            await copyDir(path.join(targetDir, file), path.join(newDir, file));
+        }
+    }
+}
+
+async function createPage() {
+    await fsPromise.mkdir(projectDist, { recursive: true});
+    await clearDir(projectDist);
+    await copyDir(path.join(__dirname, 'assets'), assetsDir);
+
+    let writeStream = fs.createWriteStream(stylesFile);
+    let cssFiles = await fsPromise.readdir(path.resolve(__dirname, 'styles'));
+    for (let file of cssFiles) {
+        if (path.extname(file) === '.css') {
+            let readStream = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf8');
+            readStream.on('data', (chunk) => {
+                writeStream.write(chunk + '\n');
             })
         }
     }
-});
 
-fs.readdir(path.resolve(__dirname, 'components'), { withFileTypes: true }, (_, files) => {
-    let text = '';
-    fs.readFile(path.resolve(__dirname, 'template.html'), { encoding: 'utf-8' }, (ee, data) => {
-        text = data;
-    });
-    for (let file of files) {
-        if (file.isFile() && path.extname(file.name) === '.html') {
-            fs.readFile(path.join(__dirname, 'components', `${file.name}`), { encoding: 'utf-8' }, (ee, data) => {
-                text = text.replace(`{{${file.name.split('.')[0]}}}`, `${data}`);
-                fs.writeFile(htmlFile, text, (err) => {
-                    if (err) console.log(err);
-                })
-            });
+    await fsPromise.copyFile(path.join(__dirname, 'template.html'), htmlFile);
+
+    let htmlText = await fsPromise.readFile(htmlFile, 'utf8');
+    let htmlFiles = await fsPromise.readdir(path.resolve(__dirname, 'components'));
+    for (let file of htmlFiles) {
+        if (path.extname(file) === '.html') {
+            let readStream = fs.createReadStream(path.join(__dirname, 'components', file), 'utf8');
+            readStream.on('data', (chunk) => {
+                htmlText = htmlText.replace(`{{${file.split('.')[0]}}}`, chunk);
+                let writeStream = fs.createWriteStream(htmlFile);
+                writeStream.write(htmlText);
+            })
         }
     }
+}
 
-});
+createPage();
+
+
+
+
 
